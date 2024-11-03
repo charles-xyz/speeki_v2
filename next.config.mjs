90f10ecbf2646c234489464e1a4e0027115ab444
charles-xyz <charleslazaroni@gmail.com>
2024-11-03 13:56:05 -0800

    fixing ts errors 5

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-  }
+
 export default nextConfig;
